f2a62218e07139549514a64eba2435be504e076a
Marcus Kaufmann <m.kaufmann@xceptance.net>
2015-12-15 14:42:26 +0100

    edited to run in annotation runner. added some testscenarios

--- src/tests/navigation/mobile/TMobile_ValidateMobileNavigation.java
+++ src/tests/navigation/mobile/TMobile_ValidateMobileNavigation.java
@@ -2,13 +2,21 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package tests.navigation.mobile;
-import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
+import tests.augment.AbstractAnnotatedScriptTestCase;
+import tests.augment.annotation.TestTarget;
+import tests.augment.annotation.TestTargets;
+import tests.augment.enums.Browser;
 
 /**
- * <p>Test is for mobile devices only - it uses mobile menu to navigate to the catalog page</p>
- * <p>This test case will only work when your browser window is smaller than 500 pixel</p>
+ * <p>
+ * Test is for mobile devices only - it uses mobile menu to navigate to the catalog page
+ * </p>
+ * <p>
+ * This test case will only work when your browser window is smaller than 500 pixel
+ * </p>
  * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
  * <ul>
  * <li>open site, delete cookies , open it again</li>
@@ -21,8 +29,16 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  * <li>close mobile menu</li>
  * </ul>
  */
-@ScriptName
-("tests.navigation.mobile.TMobile_ValidateMobileNavigation")
-public class TMobile_ValidateMobileNavigation extends AbstractScriptTestCase
+@ScriptName("tests.navigation.mobile.TMobile_ValidateMobileNavigation")
+@TestTargets({
+    @TestTarget(browser = Browser.MobileGalaxyNote2, testCaseName = "Galaxy Not 2"), //
+    @TestTarget(browser = Browser.MobileIphone6Plus), //
+//    @TestTarget(browser = Browser.MobileGalaxyS3), //
+//    @TestTarget(browser = Browser.MobileGalaxyS4), //
+//    @TestTarget(browser = Browser.MobileIphone4), //
+//    @TestTarget(browser = Browser.MobileIphone6Plus), //
+//    @TestTarget(browser = Browser.MobileNexus6), //
+})
+public class TMobile_ValidateMobileNavigation extends AbstractAnnotatedScriptTestCase
 {
 }
