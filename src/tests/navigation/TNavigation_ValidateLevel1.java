7ecddac699b4a3800ea8a5c4e1c4c4008b39dbf5
Marcus Kaufmann <m.kaufmann@xceptance.net>
2015-12-15 14:46:42 +0100

    edited

--- src/tests/navigation/TNavigation_ValidateLevel1.java
+++ src/tests/navigation/TNavigation_ValidateLevel1.java
@@ -2,12 +2,18 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package tests.navigation;
-import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
+import tests.augment.AbstractAnnotatedScriptTestCase;
+import tests.augment.annotation.TestTarget;
+import tests.augment.annotation.TestTargets;
+import tests.augment.enums.Browser;
 
 /**
- * <p>Test opens Level 1 Categories and validates Catalog Page</p>
+ * <p>
+ * Test opens Level 1 Categories and validates Catalog Page
+ * </p>
  * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
  * <ul>
  * <li>Start session, open homepage and delete all visible cookies</li>
@@ -20,8 +26,11 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
  * <li>validate Page</li>
  * </ul>
  */
-@ScriptName
-("tests.navigation.TNavigation_ValidateLevel1")
-public class TNavigation_ValidateLevel1 extends AbstractScriptTestCase
+@ScriptName("tests.navigation.TNavigation_ValidateLevel1")
+@TestTargets(
+    {
+        @TestTarget(browser = Browser.Firefox)
+    })
+public class TNavigation_ValidateLevel1 extends AbstractAnnotatedScriptTestCase
 {
 }
