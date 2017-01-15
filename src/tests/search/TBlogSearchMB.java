dbf96cf074b7c07c1e066545f57e8027659dd345
Rene Schwietzke <r.schwietzke@xceptance.net>
2017-01-15 16:21:14 +0100

    Test case cleanup and simplification

---
+++ src/tests/search/TBlogSearchMB.java
@@ -0,0 +1,21 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.search;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+import xltutil.AbstractAnnotatedScriptTestCase;
+import xltutil.annotation.TestTargets;
+
+
+/**
+ * <p>This is a simple demo for a Google search and will demonstrate how the multi browser suite works.</p>
+ */
+@ScriptName("tests.search.TBlogSearch")
+@TestTargets(
+                {
+                    "chrome_sl", "FF_1000x768", "FF_1500x1000", "Chrome_1280x900", "Chrome_1500x1000", "Galaxy_Note3_Emulation"
+                })
+public class TBlogSearchMB extends AbstractAnnotatedScriptTestCase
+{
+}
